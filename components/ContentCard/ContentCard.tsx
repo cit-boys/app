b5212f42482e96ac3d41fbd8b17b83ccac353270
Orlan Quijada <orlantquijada@gmail.com>
2021-03-22 16:30:20 +0800

    content card

---
+++ components/ContentCard/ContentCard.tsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.scss'
+
+interface Props {
+  title: string
+  subtitle?: string
+  rightComponent: React.ReactElement
+  children: React.ReactElement
+}
+
+export default function ContentCard({
+  title,
+  subtitle,
+  children,
+  rightComponent,
+}: Props): React.ReactElement {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div>
+          <h1 className={styles.title}>{title}</h1>
+          <h4 className={styles.subtitle}>{subtitle}</h4>
+        </div>
+
+        {rightComponent}
+      </div>
+      {children}
+    </div>
+  )
+}
