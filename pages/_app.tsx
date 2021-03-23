a04ce7c1a6798fd294426e25231f23dad3afe8bc
Orlan Quijada <orlantquijada@gmail.com>
2021-03-23 20:37:39 +0800

    aksjd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,21 @@
 import { AppProps } from 'next/app'
-// import { useRouter } from 'next/dist/client/router'
-// import Head from 'next/head'
+import { useRouter } from 'next/dist/client/router'
+import Head from 'next/head'
 import Layout from '../components/Layout'
 
 import '../styles/global.scss'
 
-// const toTitleCase = (str: string) => `${str[1].toUpperCase()}${str.slice(2)}`
+const toTitleCase = (str: string) => `${str[1].toUpperCase()}${str.slice(2)}`
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  // const router = useRouter()
+  const router = useRouter()
 
   return (
     <>
-      {/* <Head>
+      <Head>
         <title>{toTitleCase(router.pathname || 'home')} | MySalary.fyi</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head> */}
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
